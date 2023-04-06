72e7a6611297f94e513ec1e779f4d3240b687928
PedroMizani <pedromizani@outlook.com>
2023-04-06 11:07:49 -0300

    Exercicio Metodos Abstratos curso c#

---
+++ ExeMetAbs/Entities/Individual.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExeMetAbs.Entities
+{
+     class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual() { }
+
+        public Individual(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double Tax()
+        {
+
+            double tax = 0;
+
+            if (HealthExpenditures > 0)
+            {
+                if (AnualIncome < 20.000)
+                {
+                    tax = AnualIncome * 0.15 - HealthExpenditures * 0.50;
+                }
+            }
+
+            if (HealthExpenditures > 0)
+            {
+                if (AnualIncome >= 20.000)
+                {
+                    tax = AnualIncome * 0.25 - HealthExpenditures * 0.50;
+                }
+            }
+
+            else if (HealthExpenditures < 0)
+            {
+                if (AnualIncome < 20.000)
+                {
+                    tax = AnualIncome * 0.15;
+                }
+            }
+
+            else if (HealthExpenditures < 0)
+            {
+                if (AnualIncome > 20.000)
+                {
+                    tax = AnualIncome * 0.25;
+                }
+            }
+
+            return tax;
+
+        }
+    }
+}
+
---
+++ ExeMetAbs/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using ExeMetAbs.Entities;
+using System.Collections.Concurrent;
+using System.Globalization;
+
+namespace ExeMetAbs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of tax payers: ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<TaxPayer> list = new List<TaxPayer>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Tax payer #{i} data: ");
+
+                Console.Write("Individual or company (i/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Anual income: ");
+                double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (ch == 'i')
+                {
+                    Console.Write("Health expenditures: ");
+                    double healthExpenditures = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                    list.Add(new Individual(name, anualIncome, healthExpenditures));
+                }
+
+                else
+                {
+                    Console.Write("Number of employees: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+
+                    list.Add(new Company(name, anualIncome, numberOfEmployees));
+                }
+            }
+            double totalPax = 0.0;
+            Console.WriteLine();
+            Console.WriteLine("TAXES PAID:");
+
+            foreach (TaxPayer taxPayer in list)
+            {
+               Console.WriteLine(taxPayer.Name + ": $ " + taxPayer.Tax().ToString("F2",CultureInfo.InvariantCulture));
+                double tax = taxPayer.Tax();
+                totalPax += tax;
+            }
+            Console.WriteLine();
+            Console.WriteLine("TOTAL TAXES: $ " + totalPax.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
